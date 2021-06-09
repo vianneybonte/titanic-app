8241a8e4c3288b1c341107a9f79811facbe3b690
vianneybonte <62135966+vianneybonte@users.noreply.github.com>
2021-06-09 15:30:04 +0200

    Add files via upload

--- interface.py
+++ interface.py
@@ -18,7 +18,6 @@ Envie de savoir si vous auriez survécu au Titanic ? Plutôt Rose ou Jack ?
 * **Python libraries:** pandas, streamlit, pickle, sklearn
 """)
 
-
 st.sidebar.image(r'images/titanic.jpg', width=300)
 
 sexe_lbl = st.sidebar.selectbox('Homme ou Femme ?', ["Homme", "Femme"])
@@ -27,12 +26,10 @@ age = st.sidebar.slider('Quel âge avez vous ?', 1, 110, 1, 1)
 famille = st.sidebar.slider('Combien de menbre de votre famille à aider ?', 1, 6, 1, 1)
 fare = st.sidebar.slider('Prix de votre ticket ?', 1, 250, 1, 1)
 
-
 if sexe_lbl =="Homme":
     sexe = 0
 else:
     sexe = 1
-
 
 if richesse_lbl =="Riche":
     Pclass = 1
@@ -42,10 +39,8 @@ else :
     Pclass = 3
 
 
-Pkl_Filename = r"model_titanic.pkl"  
+Pkl_Filename = r"modele_knn_80.pkl"  
 
-
-# Load the Model back from file
 with open(Pkl_Filename, 'rb') as file:  
     pickled_model = pickle.load(file)
 
@@ -53,32 +48,20 @@ with open(Pkl_Filename, 'rb') as file:
 st.write("Le modèle utilisé :")
 pickled_model
 
-st.write("Ce modèle est fiable à 78% ")
-
-
-
-
+st.write("Ce modèle est fiable à 80% ")
 
 data = {'pclass':[Pclass],'sex':[sexe],'age':[age], 'sibsp':[famille], 'fare':[fare]}
 df = pd.DataFrame(data=data)
 
-
-
 df = MinMaxScaler().fit_transform(df)
-print(df)
 
 df = pd.DataFrame(df, columns = ['pclass', 'sex', 'age', 'sibsp', 'fare'])
 
-
 df.dropna(axis=0, inplace=True)
-
 
 Ypredict = pickled_model.predict(df)  
 
 
-
-
-        
 if Ypredict == 1:
     st.write("Le résultat :")
     col1, mid, col2 = st.beta_columns([1,1,200])
@@ -88,7 +71,7 @@ if Ypredict == 1:
         st.write('Bravo ! Vous survivez !')
 
 
-else:
+elif Ypredict == 0:
     st.write("Le résultat :")
     col1, mid, col2 = st.beta_columns([1,1,200])
     with col1:
